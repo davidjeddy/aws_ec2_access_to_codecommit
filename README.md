ca6d0b84e1ca4e8a91b17a70c4629be0199a65a7
davidjeddy <me@davidjeddy.com>
2019-03-27 22:02:41 -0400

    Cont work on EC2 to CodeCommit access via role and policy.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
+# README
 This contains simple terraform scripts, to create an iam role, an iam policy, spin up a new ec2 instance and attach it to that instance. 
 This was completely done based on the samples provided in the official documentation of Terraform.
+
+## Terraform Commands
+
+``` bash
+terraform init
+terraform plan
+terraform apply
+```
+
+## Bash Commands
+
+### CentOS / RHEL
+
+``` bash
+sudo apt-get purge aws
+sudo apt-get install python-pip
+sudo pip install awscli
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+```
+
+``` bash
+sudo apt-get purge aws
+sudo apt-get install python-pip
+sudo pip install awscli
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+```
--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "my-test-instance" {
   ami           = "ami-b73b63a0"
   instance_type = "t2.micro"
-
-#   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
+  key_name      = "test_key"
 
   tags {
     Name = "test-instance"
@@ -14,27 +13,8 @@ resource "aws_iam_role" "ec2_s3_access_role" {
   assume_role_policy = "${file("./policies/assumerolepolicy.json")}"
 }
 
-# resource "aws_iam_policy" "policy" {
-#   name        = "test-policy"
-#   description = "A test policy"
-#   policy      = "${file("./policies/policys3bucket.json")}"
-# }
-
-# only use ONE of the following TWO options
-# resource "aws_iam_policy_attachment" "test-attach" {
-#   name       = "test-attachment"
-#   roles      = ["${aws_iam_role.ec2_s3_access_role.name}"]
-#   policy_arn = "${aws_iam_policy.policy.arn}"
-# }
-
 resource "aws_iam_role_policy_attachment" "test-attach" {
 #  name       = "test-attachment"
   role      = "${aws_iam_role.ec2_s3_access_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitReadOnly"
 }
-
-# resource "aws_iam_instance_profile" "test_profile" {
-#   name = "test_profile"
-#   role = "${aws_iam_role.ec2_s3_access_role.name}"
-# }
-
